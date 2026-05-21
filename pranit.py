d2965d7545beaf9b0e874cf0a6c7593f4a62042b
Pranit-Maharjan <maharjanpranit37@gmail.com>
2026-05-21 08:19:11 +0545

    fok

--- pranit.py
+++ pranit.py
@@ -1,1 +1,28 @@
-print("python is shet")
+# Numeric types
+integer_value = 10
+float_value = 3.14
+
+# Text type
+string_value = "Hello, world!"
+
+# Boolean type
+boolean_value = True
+
+# Collection types
+list_value = [1, 2, 3]
+tuple_value = (4, 5, 6)
+dict_value = {"name": "Pranit", "age": 20}
+set_value = {"apple", "banana", "cherry"}
+
+# Special type
+none_value = None
+
+print(type(integer_value))
+print(type(float_value))
+print(type(string_value))
+print(type(boolean_value))
+print(type(list_value))
+print(type(tuple_value))
+print(type(dict_value))
+print(type(set_value))
+print(type(none_value))
