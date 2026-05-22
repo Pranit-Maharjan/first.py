ef144f3119a5bb014e904f8b28c3234e9889181f
Pranit-Maharjan <maharjanpranit37@gmail.com>
2026-05-22 08:11:34 +0545

    dict

---
+++ dict.py
@@ -0,0 +1,15 @@
+student = {
+    "name": "Hotoke",
+    "age": 21,
+    "course": "BCSIT",
+    }
+print(student["name"])
+print(student["age"])
+print(student["course"])
+student["age"] = 22
+student["college"] = "ABC College"
+print(student)
+del student["course"]
+print(student)
+
+
--- list.py
+++ list.py
@@ -1,7 +1,20 @@
 numbers = [10, 20 ,30, 40, 50]
-print (numbers[0]) 
-print (numbers[2])
-print (numbers[5])
+print(numbers[0]) 
+print(numbers[2])
+print(numbers[4])
 
-print (numbers[-1])
-print (numbers[-3])
+print(numbers[-1])
+print(numbers[-3])
+
+fruits =["apple", "banana", "cherry", "oranges", "grapes"]
+print(fruits[1])
+print(fruits[-2])
+
+numbers = [10, 20, 30, 40, 50]
+print(numbers[1:4])
+
+print(numbers[::-1])
+
+numbers =("10, 11, 12, 13, 14, 15")
+print(numbers[0])
+print(numbers[1:4])
